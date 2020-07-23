66e0095623582880d987ecbaa94ae993995068a8
Markus Blaschke <mblaschke82@gmail.com>
2020-07-23 02:01:27 +0200

    Add multiple match test
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- config/config_test.go
+++ config/config_test.go
@@ -93,3 +93,38 @@ func Test_NodeLabelMatcher(t *testing.T) {
 		t.Error("Expected matching, but not matching node")
 	}
 }
+
+
+func Test_NodeMultipleMatcher(t *testing.T) {
+	node := buildNode()
+
+	pool := PoolConfig{
+		Name: "testing",
+		Selector: []PoolConfigSelector{
+			{
+				Path:  "{.spec.providerID}",
+				Match: stringPtr("azure:///subscriptions/d86bcf13-ddf7-45ea-82f1-6f656767a318/resourceGroups/mc_k8s_mblaschke_westeurope/providers/Microsoft.Compute/virtualMachineScaleSets/aks-agents-35471996-vmss/virtualMachines/30"),
+			},
+			{
+				Path:  "{.metadata.labels.node\\.kubernetes\\.io/role}",
+				Match: stringPtr("worker"),
+			},
+		},
+	}
+	matching, err := pool.IsMatchingNode(node)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if !matching {
+		t.Error("Expected matching, but not matching node")
+	}
+
+	node.Spec.ProviderID = "testing"
+	matching, err = pool.IsMatchingNode(node)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if matching {
+		t.Error("Expected not matching, but matching node")
+	}
+}
